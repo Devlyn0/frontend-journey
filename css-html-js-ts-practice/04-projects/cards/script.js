3912a3935981179c4871959e1c9ac46847f0f61c
Apple <apple@MacBook-Pro-Apple.local>
2026-03-25 20:58:16 +0700

    Make a new project to learn grid

--- css-html-js-ts-practice/04-projects/cards/script.js
+++ css-html-js-ts-practice/04-projects/cards/script.js
@@ -1,11 +1,22 @@
+let contacts_close_btn = document.querySelector('.close_button');
 let contacts_open_btn = document.querySelector('.contacts_button');
-contacts_open_btn.addEventListener('click', () => {
+let overlay = document.querySelector('.overlay');
+
+
+const openContacts = () => {
     let contacts_menu = document.querySelector('.contacts_menu');
     contacts_menu.style.visibility = 'visible';
-})
+    overlay.classList.add('animated');
+    document.body.style.overflow = 'hidden';
+}
 
-let contacts_close_btn = document.querySelector('.close_button');
-contacts_close_btn.addEventListener('click', () => {
+const closeContacts = () => {
     let contacts_menu = document.querySelector('.contacts_menu');
     contacts_menu.style.visibility = 'hidden';
-})
+    overlay.classList.remove('animated');
+    document.body.style.overflow = '';
+}
+
+contacts_open_btn.addEventListener('click', openContacts);
+contacts_close_btn.addEventListener('click', closeContacts);
+overlay.addEventListener('click', closeContacts)
